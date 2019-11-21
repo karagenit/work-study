f077a38a2ff513015ebd950096211ea20aef2bce
Caleb Smith <karagenit@outlook.com>
2019-11-20 23:56:39 -0500

    Remove HTML Tags from CSV Output

--- parse.rb
+++ parse.rb
@@ -10,6 +10,6 @@ matches = page.scan(/<td>(.*?)<tr>/).to_a
 CSV.open("jobs.csv", "w") do |csv|
   puts "Found #{matches.size} postings"
   matches.each do |posting|
-    csv << posting[0].scan(/(?<=<\/b>)(.*?)(?<=<b>)/).to_a.map { |a| a[0] }
+    csv << posting[0].scan(/(?<=<\/b>)(.*?)(?<=<b>)/).to_a.map { |a| a[0].gsub(/<[^>]+>/, '').chomp }
   end
 end
