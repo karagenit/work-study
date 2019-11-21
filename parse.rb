63e4c6e20b69f07eb0d922b278898f297d6c10ed
Caleb Smith <karagenit@outlook.com>
2019-11-20 23:53:40 -0500

    Add Parse Script

---
+++ parse.rb
@@ -0,0 +1,15 @@
+require 'csv'
+
+#page = File.read('JobSearch.php')
+#page = page.encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
+
+page = File.read('jobs.html')
+
+matches = page.scan(/<td>(.*?)<tr>/).to_a
+
+CSV.open("jobs.csv", "w") do |csv|
+  puts "Found #{matches.size} postings"
+  matches.each do |posting|
+    csv << posting[0].scan(/(?<=<\/b>)(.*?)(?<=<b>)/).to_a.map { |a| a[0] }
+  end
+end
